f4a3cdd0c877af3810e02df4dfcffd270693aa02
Liviu Preda <liviupreda@gmail.com>
2019-09-21 14:11:43 +0300

    update current score and switch players when 1 rolled

--- app.js
+++ app.js
@@ -4,25 +4,31 @@
 // HTML & CSS ©Jonas Schmedtman
 //************************************/
 
-let scores, roundScore, currentPlayer;
+let scores, currScore, currPlayer;
 
 scores = [0, 0]; // Array for P1 and P2 scores
-roundScore = 0;
-currentPlayer = 0; // P1 = 0 ; P2 = 1
+currScore = 0;
+currPlayer = 0; // P1 = 0 ; P2 = 1
 
 // Setter; sets the textContent value
-document.getElementById('current-' + currentPlayer).textContent = dice;
+// document.getElementById('current-' + currentPlayer).textContent = dice;
 
 // document.getElementById('current-' + currentPlayer).innerHTML = `
 //   <em>${dice}</em>
 // `;
 
-// Getter; gets the round score textContent value
-let x = document.getElementById('score-0').textContent;
-console.log(x);
+// Getter; gets the total score textContent value
+// let x = document.getElementById('score-0').textContent;
+// console.log(x);
 
 // Hide dice image at the beginning
 document.querySelector('.dice').style.display = 'none';
+
+// Reset the current and total scores for P1 and P2
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
 // Roll dice
 document.querySelector('.btn-roll').addEventListener('click', rollDice);
@@ -30,5 +36,38 @@ document.querySelector('.btn-roll').addEventListener('click', rollDice);
 function rollDice() {
   const dice = Math.floor(Math.random() * 6) + 1; // Random integer between 1 and 6
   // Display dice roll
-  const diceToDOM = (document.querySelector('.dice').style.display = 'block');
+  const diceDOM = document.querySelector('.dice');
+  diceDOM.style.display = 'block';
+  diceDOM.src = `dice-${dice}.png`;
+
+  // Update current score only IF the rolled number != 1
+  if (dice !== 1) {
+    // Display & Add current score to current score
+    currScore += dice;
+    document.getElementById('current-' + currPlayer).textContent = currScore;
+  } else {
+    // Switch player
+    if (currPlayer === 0) {
+      document
+        .querySelector('.player-' + currPlayer + '-panel')
+        .classList.remove('active');
+      currPlayer = 1;
+    } else {
+      document
+        .querySelector('.player-' + currPlayer + '-panel')
+        .classList.remove('active');
+      currPlayer = 0;
+    }
+
+    document
+      .querySelector('.player-' + currPlayer + '-panel')
+      .classList.add('active');
+    // Hide dice
+    diceDOM.style.display = 'none';
+    // Reset current score for current player
+    currScore = 0;
+    document.getElementById(
+      'current-' + currPlayer
+    ).textContent = `${currScore}`;
+  }
 }
